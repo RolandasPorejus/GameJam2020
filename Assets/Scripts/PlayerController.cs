cfcbdfbcdbcd3c338e45fdf17b9936ea8cdd5651
RolandasPorejus <rolandas.p1996@gmail.com>
2020-02-01 15:05:00 +0200

    Added jumping and carryable objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,43 +6,72 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private GameObject camera;
+    //private GameObject camera;
 
-    public float speed;
     public float moveSpeed;
+    public float jumpForce;
+    Vector3 jump;
+    bool jumping = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        camera = GameObject.Find("Main Camera");
-
-        moveSpeed = 4.0F;
-        speed = moveSpeed;
+        //camera = GameObject.Find("Main Camera");
+        //rb.inertiaTensor = rb.inertiaTensor + new Vector3(0, 0, rb.inertiaTensor.z * 100);
     }
 
     void Update()
     {
-        speed = moveSpeed;
-
-        float horMove = speed * Input.GetAxis("Horizontal");
-        float vertMove = speed * Input.GetAxis("Vertical");
+        float horMove = moveSpeed * Input.GetAxis("Horizontal");
+        float vertMove = moveSpeed * Input.GetAxis("Vertical");
 
         if (horMove != 0 || vertMove != 0)
         {
             var move_vec = new Vector3(horMove, 0, vertMove);
-            Debug.Log(move_vec);
+            //Debug.Log(move_vec);
 
             move_vec = Camera.main.transform.TransformDirection(move_vec);
-            move_vec.y = 0.0f;
-            rb.velocity = move_vec;
+
+            Vector3 v = rb.velocity;
+            v.x = move_vec.x;
+            v.z = move_vec.z;
+            rb.velocity = v;
         }
 
-        if (horMove == 0 && vertMove == 0) {
+        if (CanJump() && Math.Abs(rb.velocity.y) < 0.01)
+        {
+            jumping = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (CanJump())
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                jumping = true;
+            }
+        }
+
+
+        if (horMove == 0 && vertMove == 0 && !jumping)
+        {
             rb.velocity = Vector3.zero;
         }
     }
 
-    void MovePlayer(float horizontal, float vertical)
+    bool CanJump()
+    {
+        Ray ray = new Ray(transform.position, transform.up * -1);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, transform.localScale.y + 0.2f))
+        {
+            return true;
+        }
+        return false;
+    }
+
+        void MovePlayer(float horizontal, float vertical)
     {
         //transform.localEulerAngles = Camera.main.transform.localEulerAngles;
         /*
---
+++ Assets/Scripts/PlayerPickup.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPickup : MonoBehaviour
+{
+    GameObject mainCamera;
+    GameObject player;
+    GameObject carryPosition;
+    GameObject carriedObject;
+    GameObject pickupText;
+    GameObject dropText;
+    public float pickupDistance;
+    bool carrying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        carryPosition = GameObject.Find("CarryPosition");
+        pickupText = GameObject.Find("PickupText");
+        dropText = GameObject.Find("DropText");
+        player = GameObject.Find("Player");
+
+        pickupText.SetActive(false);
+        dropText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        int x_center = Screen.width / 2;
+        int y_center = Screen.height / 2;
+
+        Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x_center, y_center));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject selectedObject = hit.transform.gameObject;
+            if (selectedObject.tag == "Carry" && Vector3.Distance(mainCamera.transform.position, selectedObject.transform.position) < pickupDistance)
+            {
+                pickupText.SetActive(true);
+            }
+            else
+            {
+                pickupText.SetActive(false);
+            }
+        }
+        else {
+            pickupText.SetActive(false);
+        }
+
+        if (carrying) {
+            pickupText.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (carrying)
+            {
+                Drop();
+                
+            }
+            else
+            {
+                carriedObject = hit.transform.gameObject;
+                Pickup(carriedObject);
+            }
+        }
+
+        if (carrying)
+        {
+            carriedObject.transform.position = carryPosition.transform.position;
+            carriedObject.transform.rotation = carryPosition.transform.rotation;
+            carriedObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    void Pickup(GameObject carriedObject)
+    {
+        carrying = true;
+        pickupText.SetActive(false);
+        dropText.SetActive(true);
+        carriedObject.GetComponent<Rigidbody>().useGravity = false;
+        carriedObject.transform.position = carryPosition.transform.position;
+    }
+
+    void Drop()
+    {
+        carrying = false;
+        dropText.SetActive(false);
+        carriedObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
